d7dd1b840eeab884f07fb25841d586b9c7d28fb6
Sam Kompfner <adm.samuel.kompfner@sra.org.uk>
2020-09-21 15:41:03 +0100

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var book = new Book("Sam's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.GetStatistics();
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            
+
+        }
+    }
+}
